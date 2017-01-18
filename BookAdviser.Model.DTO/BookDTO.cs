373a6402bc3222a722832814a5e535aedef8d2db
Sinem Sarı <sinem_sari@windowslive.com>
2017-01-18 18:03:00 +0200

    Service and DTO Layers added

---
+++ BookAdviser.Model.DTO/BookDTO.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookAdviser.Model.DTO
+{
+    public class BookDTO
+    {
+        //public static implicit operator BookDTO(Book book)
+        //{
+        //    BookDTO dto = new BookDTO
+        //    {
+        //        Name = book.Name,
+        //        AuthorName = book.Author.Name,
+        //        Publisher = book.Publisher.Name,
+        //        Abstract = book.Abstract,
+        //        Language = book.Language.Name,
+        //        PublishYear = book.PublishYear,
+        //        Price = book.Price,
+        //        ISBN = book.ISBN,
+        //    };
+
+        //    if (book.Translator != null)
+        //    {
+        //        dto.TranslatorName = book.Translator.Name;
+        //    }
+
+
+        //    return dto;
+        //}
+
+        //public static explicit operator BookDTO(Book book)
+        //{
+        //    BookDTO dto = new BookDTO
+        //    {
+        //        Name = book.Name,
+        //        AuthorName = book.Author.Name,
+        //        Publisher = book.Publisher.Name,
+        //        Abstract = book.Abstract,
+        //        Language = book.Language.Name,
+        //        PublishYear = book.PublishYear,
+        //        Price = book.Price,
+        //        ISBN = book.ISBN,
+        //    };
+
+        //    if (book.Translator != null)
+        //    {
+        //        dto.TranslatorName = book.Translator.Name;
+        //    }
+
+
+        //    return dto;
+        //}
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public string Publisher { get; set; }
+        public string Abstract { get; set; }
+        public string Language { get; set; }
+        public string TranslatorName { get; set; }
+        public Nullable<int> PublishYear { get; set; }
+        public string ISBN { get; set; }
+        public Nullable<decimal> Price { get; set; }
+    }
+
+}
---
+++ BookAdviser.Service/BookService.cs
@@ -0,0 +1,34 @@
+﻿using BookAdviser.Model.DTO;
+using BookAdviser.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookAdviser.Service
+{
+    public class BookService
+    {
+        BookAdviserUnitOfWork _uow;
+        public BookService()
+        {
+            _uow = new BookAdviserUnitOfWork();
+        }
+
+        public IEnumerable<BookDTO> GetBookList()
+        {
+            return _uow.Books.GetAll().Select(x => new BookDTO{
+                Name = x.Name,
+                AuthorName = x.Author.Name,
+                Publisher = x.Publisher.Name,
+                Abstract = x.Abstract,
+                Language = x.Language.Name,
+                PublishYear = x.PublishYear,
+                Price = x.Price,
+                ISBN = x.ISBN,
+                TranslatorName = x.Translator == null ? null : x.Translator.Name 
+            });
+        }
+    }
+}
